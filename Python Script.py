ec967d12d1583b74ebe5bab86a475cd9faa45404
333Vaibhav <vaibhavrathod19990@gmail.com>
2025-07-27 10:06:41 +0530

    Add files via upload

---
+++ Python Script.py
@@ -0,0 +1,24 @@
+#TASK1 Performed Basic Mathematical Operations
+
+num1 = int(input("Enter the value of num1: "))
+num2 = int(input("Enter the value of num2: "))
+
+Add = num1 + num2
+print("Addition is :",Add)
+
+Sub = num1 - num2
+print("Substraction is :",Sub)
+
+Mul = num1 * num2
+print("Multiplication is :",Mul)
+
+Div = num1 / num2
+print("Division is :",Div)
+
+#TASK2 Create personalize greeting
+
+FirstName = str(input("Enter the First Name: "))
+LastName = str(input("Enter the Last Name: "))
+
+FullName = FirstName +" " +LastName
+print("Hello,",FullName,"Welcome to the Python World.")
